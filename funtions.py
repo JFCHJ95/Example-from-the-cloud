67755ebeb380589354ccb4dfa1495009c93c8903
Jonathan <chavez.jonathan5@yahoo.es>
2022-06-21 13:02:14 -0500

    update Poetry

---
+++ funtions.py
@@ -0,0 +1,12 @@
+from numpy import void
+
+
+def say_name(name: str = "Jonathan") -> void:
+    """
+    This is a funtion that prints your name
+    """
+    print(name)
+
+
+say_name()
+say_name("Ivan")
